7604f524120c7d7ca59aed12e8c405dc2d49764e
ssss <aaaaa>
2019-04-10 15:59:18 -0500

    Creacion metodo http post y get

--- 01-http/02-servidor-web-nodejs/api-web/src/app.controller.ts
+++ 01-http/02-servidor-web-nodejs/api-web/src/app.controller.ts
@@ -1,12 +1,41 @@
-import { Controller, Get } from '@nestjs/common';
+import {Controller, Get, Post} from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
+/* @[nombre] nombre del decorador y pueden utilizarse antes de instanciar clases, atributos,
+ métodos y/o constructores.
+ Un decorador es una función que se ejecuta antes de crearse la clase.
+ Pueden ser utilizados en clases, atributos, parámetros y/o métodos*/
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+    constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+    @Get()      //Método http
+    getHello(): string {
+        return this.appService.getHello();
+    }
+
+    @Post()     //Método http
+    postHello(){
+        return 'Hola mundo';
+    }
 }
+
+/*
+class usuario{
+
+
+    atributoPublico;
+    private atributoPrivado;
+    private atributoProtegido;
+
+    constructor(atributoPublico, atributoPrivado, atributoProtegido) {
+        this.atributoPublico = atributoPublico;
+        this.atributoPrivado = atributoPrivado;
+        this.atributoProtegido = atributoProtegido;
+    }
+
+    public metodoPublico(){}
+    private metodoPrivado(){}
+    protected metodoProtegido(){}
+}
+*/
