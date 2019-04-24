6b9dd2d52f2b66bddbda39a79563c496b78c18ea
Cristian Betancourt <cristianbb98@gmail.com>
2019-04-24 15:59:21 -0500

    Creacion de vistas e implementación de métodos para recorrer arreglos

--- 01-http/02-servidor-web-nodejs/api-web/dist/src/main.js
+++ 01-http/02-servidor-web-nodejs/api-web/dist/src/main.js
@@ -15,6 +15,7 @@ function bootstrap() {
     return __awaiter(this, void 0, void 0, function* () {
         const app = yield core_1.NestFactory.create(app_module_1.AppModule);
         app.use(cookieParser('Secreto'));
+        app.set('view engine', 'ejs');
         yield app.listen(3000);
     });
 }
