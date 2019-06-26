25a74c2163ddd699e460f054a5ff2469ec9667ed
Cristian Betancourt <cristianbb98@gmail.com>
2019-06-26 14:20:37 -0500

    Sesiones

--- 01-http/02-servidor-web-nodejs/api-web/dist/src/main.js
+++ 01-http/02-servidor-web-nodejs/api-web/dist/src/main.js
@@ -11,6 +11,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 const express = require("express");
+const session = require("express-session");
 var cookieParser = require('cookie-parser');
 function bootstrap() {
     return __awaiter(this, void 0, void 0, function* () {
@@ -18,6 +19,17 @@ function bootstrap() {
         app.use(cookieParser('Secreto'));
         app.set('view engine', 'ejs');
         app.use(express.static('publico'));
+        const FileStore = require('session-file-store')(session);
+        app.use(session({
+            name: 'server-session-id',
+            secret: 'No sera de tomar un traguito',
+            resave: false,
+            saveUninitialized: true,
+            cookie: {
+                secure: false
+            },
+            store: new FileStore()
+        }));
         yield app.listen(3000);
     });
 }
